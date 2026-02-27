916f325488e04d79a3a7c3903fa89162785ee627
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-02-27 10:27:53 -0300

    Segunda Aula de C#, Sintaxe básica

---
+++ Aula01/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Diagnostics;
+
+namespace Aula01; // Endereço do projeto (NOME DO PROJETO)
+
+public class Program // Sintaxe básica
+{
+    public static void Main()
+    {
+        Console.WriteLine("Olá me chamo gabriel e estou na minha primeira aula de c# no .net");
+    }
+}
