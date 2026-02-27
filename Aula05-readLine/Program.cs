17ce176b6045bbe9e1c3342bba42b1b066f0b7b8
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-02-27 13:51:55 -0300

    Comando readline

---
+++ Aula05-readLine/Program.cs
@@ -0,0 +1,21 @@
+﻿/* ===== COMANDO READLINE ===== */
+
+// Com esse comando podemos fazer com que o cliente envie informações via console para nossa aplicação
+
+// PERGUNTA
+
+Console.WriteLine("Digite seu nome: ");
+
+// Cliente ira responder 
+
+string ?nome = Console.ReadLine();
+
+/*
+    Por sua natureza o C# não converte uma informação nulla, Porque o comando readLine não garante que ira receber alguma informação
+    Porem conseguimos flexibilizar isso no C# utilizando o ? antes do nome da variavel.
+    Isso dira para o C# que a variavel pode receber uma informação nulla
+ */
+
+
+// CONCATENAÇÃO
+Console.WriteLine($"Ola seja bem vindo {nome}");
