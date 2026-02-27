3204f54177ad4f8b0422fbd6b5afacef3e9f0aca
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-02-27 13:14:25 -0300

    Concatenacao

---
+++ Aula04-concatenacao/Program.cs
@@ -0,0 +1,21 @@
+﻿/* ===== CONCATENAÇÃO ===== */
+
+/* === FORMA SIMPLES === */
+
+// Hoje aprenderemos sobre a concatenação, nada mais e nada menos a concatenação é juntar partes de variaveis ao texto
+
+// Utilizamos o + para juntar as partes das variaveis ao texto, esse é o formato mas simples
+
+string nome = "Gabriel";
+string cidade = "Severinia SP";
+int idade = 18;
+string dataNascimento = "21/03/2007";
+
+Console.WriteLine("O " +nome+ " Mora em "+cidade);
+
+/* ==== SEGUNDO FORMATO DE CONCATENAÇÃO FORMA MODERNA ==== */
+
+// Aqui utilizamos esse formato de concatenação chamado de interpolação de string ($" texto {Nome variavel } texto {Nome variavel}");
+
+Console.WriteLine($"O {nome} tem {idade} anos e nasceu na data de {dataNascimento}");
+
