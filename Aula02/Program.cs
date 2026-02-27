a338df1a0fbdfcdf96260b92f625f419adedf9c4
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-02-27 12:01:31 -0300

    Aulas sobre variaveis

---
+++ Aula02/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula02; // Endereço do projeto (NOME DO PROJETO)
+
+public class Program // Sintaxe básica
+{
+    public static void Main()
+    {
+        string myName = "Antônio";
+    }
+}
