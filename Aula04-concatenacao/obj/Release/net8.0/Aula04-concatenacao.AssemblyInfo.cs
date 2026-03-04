fac1ccb2ea176c35f22a872caf200904f0a097bf
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-04 16:36:46 -0300

    Aula05

--- Aula04-concatenacao/obj/Release/net8.0/Aula04-concatenacao.AssemblyInfo.cs
+++ Aula04-concatenacao/obj/Release/net8.0/Aula04-concatenacao.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula04-concatenacao")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ce176b6045bbe9e1c3342bba42b1b066f0b7b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula04-concatenacao")]
---
+++ Aula06-Readline-Conversao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06_Readline_Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write($"\nOlá {nome}, Agora preciso que digite sua idade: ");
+            
+            if(int.TryParse(Console.ReadLine(), out int idade))
+            {
+                Console.WriteLine($"\nCadastro feito com sucesso {nome}, Você tem {idade} anos, Está muito novo ainda !!!");
+            }
+            else
+            {
+                Console.WriteLine("\n Idade Inválida, digite sua idade");
+            }
+        }
+    }
+}
