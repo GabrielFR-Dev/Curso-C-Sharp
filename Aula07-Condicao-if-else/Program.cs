a98a6614f3acaecbea8b99a002ab374e12e33a3d
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-04 17:00:20 -0300

    Aula07

---
+++ Aula07-Condicao-if-else/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Aula07_Condicao_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+
+
+            if (int.TryParse(Console.ReadLine(), out int idade))
+            {
+                if (idade >= 18 && idade < 60)
+                {
+                    Console.WriteLine($"\nCadastro feito com sucesso {nome} você ja tem {idade} anos");
+                }
+                else if (idade >= 60)
+                {
+                    Console.WriteLine($"\nSeja bem vindo Sr {nome} você tem {idade} anos, por isso acaba de ganhar um desconto de 30% em qualquer compra");
+                }
+                else
+                {
+                    Console.WriteLine($"\nCadastro negado {nome} você ainda tem {idade} anos");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nErro! Digite sua idade");
+            }
+        }
+    }
+}
---
+++ Aula07-Condicao-if-else/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aula07-Condicao-if-else")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aula07-Condicao-if-else")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d73dd105-10d3-4856-8100-06c1db090c3d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
