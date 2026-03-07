501ad8414d7a640937441d6e59a95e1563501abe
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-07 10:17:07 -0300

    pratica03

---
+++ Pratica03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            string nomeCompleto;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome: ");
+            sobrenome = Console.ReadLine();
+
+            nomeCompleto = nome + " " + sobrenome;
+
+            Console.WriteLine($"Ola seu nome completo é {nomeCompleto}");
+            Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
+            Console.WriteLine($"Olá {nome.ToUpper()} ");
+            Console.WriteLine($"Olá {sobrenome.ToLower()}");
+            Console.WriteLine($"Olá {nomeCompleto.ToUpper()}");
+
+
+            string fragmento = nomeCompleto.Substring(0, 7);
+
+            Console.WriteLine($"Fragmento = {fragmento}");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
