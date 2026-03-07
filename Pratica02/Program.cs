b5165bf646903eadd59f7fdc5323161117173ed8
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-07 09:52:50 -0300

    pratica02

---
+++ Pratica02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ola {0} você tem {1} anos", nome, idade);
+        }
+    }
+}
