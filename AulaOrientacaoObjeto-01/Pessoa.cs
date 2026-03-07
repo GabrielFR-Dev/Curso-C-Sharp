b5c9a07ea83b092833ed3b392ef983e3380ac940
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-07 18:36:53 -0300

    Orientação a objeto

---
+++ AulaOrientacaoObjeto-01/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaOrientacaoObjeto_01
+{
+    public class Pessoa
+    {
+        // Atributos  = características
+        // Metodos = ações
+
+        // Declaração de Atributos
+        public string nome;
+        public int idade;
+        public double peso;
+        public string sexo;
+
+
+        public Pessoa(string _nome)
+        {
+            this.nome = _nome;
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine($"{this.nome} Está Andando");
+        }
+
+        public void Comer(string _comida)
+        {
+            Console.WriteLine($"{this.nome} Está comendo " + _comida);
+        }
+    }
+}
---
+++ AulaOrientacaoObjeto-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaOrientacaoObjeto_01
+{
+    internal class Program
+    {
+        // Classes -> Objetos, Atributos, Métodos, Laço Try Catch, Método Construtor
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Instâniciando o o objeto pessoa (classse "vira" Objeto)
+
+                Pessoa p1 = new Pessoa("Gabriel");
+                p1.Comer("Pizza");
+                p1.Andar();
+
+                p1.Comer("Lazanha");
+
+                Console.WriteLine("\n");
+
+                Pessoa p2 = new Pessoa("Fernanda");
+                p2.Comer("Churrasco");
+                p2.Comer("Sorvete");
+                p2.Andar();
+
+
+                Console.ReadKey();
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Ops, deu um erro: " + ex.Message);   
+            }
+        }
+    }
+}
