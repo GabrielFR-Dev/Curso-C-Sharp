18b534c607d802e7a8b8c517d00060b62ecd29e0
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-03-15 22:19:20 -0300

    Condição social

--- Boletim-Aluno/Program.cs
+++ Boletim-Aluno/Program.cs
@@ -10,50 +10,68 @@ namespace Boletim_Aluno
     {
         static void Main(string[] args)
         {
-            string nomeAluno;
-            string nota1;
-            string nota2;
-            string nota3;
-            string nota4;
-            double resultado;
-
-
-            Console.WriteLine("==== Boletim escolar ====\n");
-
-            Console.Write("Digite o nome do aluno: ");
-            nomeAluno = Console.ReadLine().ToLower();
-
-            Console.Write($"\nDigite a primeira nota do {nomeAluno}: ");
-            nota1 = Console.ReadLine();
-
-            Console.Write($"\nDigite a segunda nota: ");
-            nota2 = Console.ReadLine();
-
-            Console.Write($"\nDigite a terceira nota: ");
-            nota3 = Console.ReadLine();
-
-            Console.Write($"\nDigite a quarta nota: ");
-            nota4 = Console.ReadLine();
-
-            resultado = (Convert.ToDouble(nota1) + Convert.ToDouble(nota2) + Convert.ToDouble(nota3) + Convert.ToDouble(nota4)) / 4;
-
-            if(resultado >= 6)
+            while (true) 
             {
-                Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
-                Console.WriteLine($"O {nomeAluno} foi aprovado, PARABÉNS !!!");
-                
+                Console.Clear();
+
+                string nomeAluno;
+                string nota1;
+                string nota2;
+                string nota3;
+                string nota4;
+                double resultado;
+
+
+                Console.WriteLine("==== Boletim escolar ====\n");
+
+                Console.Write("Digite o nome do aluno: ");
+                nomeAluno = Console.ReadLine().ToLower();
+
+                Console.Write($"\nDigite a primeira nota do {nomeAluno}: ");
+                nota1 = Console.ReadLine();
+
+                Console.Write($"\nDigite a segunda nota: ");
+                nota2 = Console.ReadLine();
+
+                Console.Write($"\nDigite a terceira nota: ");
+                nota3 = Console.ReadLine();
+
+                Console.Write($"\nDigite a quarta nota: ");
+                nota4 = Console.ReadLine();
+
+                resultado = (Convert.ToDouble(nota1) + Convert.ToDouble(nota2) + Convert.ToDouble(nota3) + Convert.ToDouble(nota4)) / 4;
+
+                if (resultado >= 6)
+                {
+                    Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
+                    Console.WriteLine($"O {nomeAluno} foi aprovado, PARABÉNS !!!");
+
+                }
+                else if (resultado > 4 && resultado < 6)
+                {
+                    Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
+                    Console.WriteLine($"O {nomeAluno} ficou de recuperação estude mais !!!");
+                }
+                else
+                {
+                    Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
+                    Console.WriteLine($"O {nomeAluno} foi reprovado, estude mais !!!");
+                }
+
+                Console.Write("\n\nPressione ENTER para continuar o programa ou digite sair para finalizar: ");
+                string resposta = Console.ReadLine().ToLower();
+
+                if (resposta == "sair")
+                {
+                    Console.WriteLine("Programa finalizado. Até mais!");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+
             }
-            else if(resultado > 4 && resultado < 6)
-            {
-                Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
-                Console.WriteLine($"O {nomeAluno} ficou de recuperação estude mais !!!");
-            }
-            else
-            {
-                Console.WriteLine($"\nA média do {nomeAluno} é: {resultado}");
-                Console.WriteLine($"O {nomeAluno} foi reprovado, estude mais !!!");
-            }
-            Console.ReadKey();
 
 
 
---
+++ Condicao-Social/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao_Social
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("==== Condição Social ====\n");
+
+                Console.Write("Digite seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("\nDigite seu salário R$: ");
+                string salario = Console.ReadLine();
+
+                if (double.TryParse(salario, out double resultado))
+                {
+                    if (resultado > 5000)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"\nMuito bom {nome} Sua renda é alta");
+                        Console.ResetColor();
+                    }
+                    else if (resultado >= 2001)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"\nBom {nome} Sua renda é média");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"\nRuim {nome} Sua renda é baixa");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Numero inválido");
+                }
+
+                Console.Write("\n\nPressione ENTER para continuar ou Digite sair para finalizar o programa: ");
+                string resposta = Console.ReadLine().ToLower();
+                
+                if(resposta == "sair")
+                { 
+                    Console.WriteLine("\n\nFinalizando o programa.. Pressione enter para sair");
+                    Console.ReadKey();
+                    break;
+                } else
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
